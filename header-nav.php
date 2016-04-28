b29920f44691bf36bdc630965b450f4907f78407
Nelson I. Gonzalez <nigm2005@gmail.com>
2016-04-27 22:54:39 -0500

    Adding dynamic menu by role

--- header-nav.php
+++ header-nav.php
@@ -1,5 +1,23 @@
+<?php $user = wp_get_current_user(); ?>
 <ul class="nav nav-pills whitespace-bottom">
-    <li role="presentation" class="active"><a href="#">Inicio</a></li>
-    <li role="presentation"><a href="#">Seccion 1</a></li>
-    <li role="presentation"><a href="#">Seccion 2</a></li>
+    <li role="presentation" <?php if ( is_front_page() ): ?>class="active"<?php endif; ?>>
+        <a href="/">Inicio</a>
+    </li>
+    <?php /*
+    <?php if ($user->ID != 0): ?>
+        <li role="presentation" <?php if ( is_page( 'activity' ) ): ?>class="active"<?php endif; ?>>
+            <a href="/activity">Actividad</a>
+        </li>
+    <?php endif; ?>
+    */ ?>
+    <?php if ($user->ID != 0): ?>
+        <li role="presentation" <?php if ( is_page( 'descargas' ) ): ?>class="active"<?php endif; ?>>
+            <a href="/descargas">Descargas</a>
+        </li>
+    <?php endif; ?>
+    <?php if ( $user->ID != 0 && (in_array('editor', $user->roles) || in_array('administrator', $user->roles)) ): ?>
+        <li role="presentation" <?php if ( is_page( 'costos' ) ): ?>class="active"<?php endif; ?>>
+            <a href="/costos">Costos</a>
+        </li>
+    <?php endif; ?>
 </ul>
--- page.php
+++ page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <div class="container">
+    <?php get_header('nav'); ?>
     <?php while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; ?>
